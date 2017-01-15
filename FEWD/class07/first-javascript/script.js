5cb4bc6768bfcd335dd62f73220eb399f95eb4c9
ljooo <laurajo@generalassemb.ly>
2017-01-15 10:28:14 -0500

    solved color one!!

---
+++ FEWD/class07/first-javascript/script.js
@@ -0,0 +1,19 @@
+// alert('This is an alert. This is anoying');
+
+// document.write('Writing to the document!');
+
+// document.write(10*10);
+// document.write(100/10);
+// document.write(50-17);
+// document.write(50+17);
+// document.write('Laura Jo Hess');
+
+var firstNum = 10* 10;
+document.write(firstNum);
+
+var myName = "My name is LJ";
+document.write(myName);
+
+// document.getElementById('title').style.color = 'red';
+document.querySelector('title').style.color = 'red';
+
