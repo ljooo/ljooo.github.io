418993d1ba2eeca3980e44f0ad82968eb42810c3
ljooo <laurajo@generalassemb.ly>
2017-01-24 21:30:48 -0500

    convert

--- FEWD/HOMEWORK/week05/temperature-converter/js/project.js
+++ FEWD/HOMEWORK/week05/temperature-converter/js/project.js
@@ -4,6 +4,10 @@ $('#convertCeltoFar').click(function() {
 	var cel = parseInt($('#cel').val());
 	$('#far').val((cel*1.8)+32);
 
+	if (cel>=80) {
+		$('h1').css('color', 'blue');
+	}
+
 })
 
 $('#convertFartoCel').click(function() {
@@ -11,6 +15,10 @@ $('#convertFartoCel').click(function() {
 	var cel = $('#cel').val();
 	var far = parseInt($('#far').val());
 	$('#cel').val((far-32)/1.8);
+
+	if (far<=2){
+		$('h1').css('color', 'red');
+	}
 })
 
 $('#reset').click(function() {
