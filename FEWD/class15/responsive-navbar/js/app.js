f949cb42b72f48888001d6e483f0d4ff21e47382
ljooo <laurajo@generalassemb.ly>
2017-02-13 10:06:22 -0500

    final p update

---
+++ FEWD/class15/responsive-navbar/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	// console.logs the window.width
+	$(window).resize(function () {
+		console.log($(window).width())
+
+		if ($(window).width() > 880){
+			$('#small-nav').hide();
+		}
+	})
+
+	$('#burger').click(function(){
+		$('#small-nav').slideToggle();
+
+	})
+
+})
+
---
+++ FEWD/final-project/js/project.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	var submitButton = 0;
+	
+	$('#submit').click(function(){
+		submitButton = submitButton + 1;
+	
+		
+	if (submitButton == 1) {
+		$('.sidetext').css('background-image', 'url(images/pic1.jpg)'); 
+		$('.quote').removeClass();
+
+		} else if (submitButton == 2) {
+		$('.sidetext').css('background-image', 'url(images/pic2.jpg)'); 
+		} else if (submitButton == 3) {
+			$('.sidetext').css('background-image', 'url(images/pic3.jpg)'); 
+		} else if (submitButton == 4) {
+		$('.sidetext').css('background-image', 'url(images/pic4.jpg)'); 
+		} else if (submitButton == 5) {
+			$('.sidetext').css('background-image', 'url(images/pic5.jpg)'); 
+		} else if (submitButton == 6) {
+		$('.sidetext').css('background-image', 'url(images/pic6.jpg)'); 
+		} else if (submitButton == 7) {
+			$('.sidetext').css('background-image', 'url(images/pic7.jpg)'); 
+		} else if (submitButton == 8) {
+		$('.sidetext').css('background-image', 'url(images/pic8.jpg)'); 
+		} else if (submitButton == 9) {
+			$('.sidetext').css('background-image', 'url(images/pic9.jpg)'); 
+		} else if (submitButton == 10) {
+		$('.sidetext').css('background-image', 'url(images/pic10.jpg)'); 
+		} else if (submitButton == 11) {
+			$('.sidetext').css('background-image', 'url(images/pic11.jpg)'); 
+		} else if (submitButton == 12) {
+		$('.sidetext').css('background-image', 'url(images/pic12.jpg)'); 
+		} else if (submitButton == 13) {
+			$('.sidetext').css('background-image', 'url(images/pic13.jpg)');
+		}
+
+	})
+	})
+
+	
+
+
+
+	
